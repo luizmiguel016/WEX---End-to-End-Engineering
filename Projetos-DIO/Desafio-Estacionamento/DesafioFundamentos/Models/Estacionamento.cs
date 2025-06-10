bb7cbb4dee638bc9b51f6bc11fff37c149b6295e
Luiz Miguel <109276669+luizmiguel016@users.noreply.github.com>
2025-06-10 16:07:13 -0300

    Arquivos Desafio do Estacionamento
    Adicionado os arquivos do Desafio do Estacionamento

---
+++ Projetos-DIO/Desafio-Estacionamento/DesafioFundamentos/Models/Estacionamento.cs
@@ -0,0 +1,72 @@
+namespace DesafioFundamentos.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        /// <summary>
+        /// Adicionar um veículo pela placa inserida pelo usuário
+        /// </summary>
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculos.Add(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Remover o veículo inserido pelo usuário, caso a placa seja existente.
+        /// </summary>
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine();
+
+
+            // Verifica se o veículo existe
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+               
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal valorTotal = precoInicial + precoPorHora * horas;
+
+                veiculos.Remove(placa);
+
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: {valorTotal:C}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        /// <summary>
+        /// Exibir a lista dos veículos estacionados
+        /// </summary>
+        public void ListarVeiculos()
+        {
+            // Verifica se há veículos no estacionamento
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                
+                foreach (string reservas in veiculos)
+                {
+                    Console.WriteLine($"{reservas}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
